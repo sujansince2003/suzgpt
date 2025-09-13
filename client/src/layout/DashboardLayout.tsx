62598ce7af644db77acce2e5a6b4dcf653bd613e
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-13 21:03:56 +0545

    auth check logic for dashboard and protected route

--- client/src/layout/DashboardLayout.tsx
+++ client/src/layout/DashboardLayout.tsx
@@ -1,6 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import Loading from "../Components/Loading";
 
 const DashboardLayout = () => {
+  const { userId, isLoaded } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoaded && !userId) {
+      navigate("/login");
+    }
+  }, [userId, isLoaded, navigate]);
+
+  if (!isLoaded) {
+    return <Loading />;
+  }
+
   return (
     <div>
       <div>Dashboard</div>
