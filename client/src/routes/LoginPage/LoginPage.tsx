6253206058ecb9fd6fe330bf82d2849341482330
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-13 21:02:53 +0545

    login and register pages

--- client/src/routes/LoginPage/LoginPage.tsx
+++ client/src/routes/LoginPage/LoginPage.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { SignIn } from "@clerk/clerk-react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className=" pt-40 flex items-center justify-center">
+      <SignIn path="/login" signInUrl="/register" />
+    </div>
+  );
 };
 
 export default LoginPage;
--- client/src/routes/SignUpPage/signupPage.tsx
+++ client/src/routes/SignUpPage/signupPage.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { SignUp } from "@clerk/clerk-react";
 
 const signupPage = () => {
-  return <div>signupPage</div>;
+  return (
+    <div className=" pt-40 flex items-center justify-center">
+      <SignUp path="/register" signInUrl="/login" />;
+    </div>
+  );
 };
 
 export default signupPage;
